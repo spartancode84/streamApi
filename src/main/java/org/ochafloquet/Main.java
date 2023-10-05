3e684ff6446a4e4c7d952379646fdc1fe337fdf0
Oscar Alfredo Chafloque Tampeck <oscar.chafloque@soytul.com>
2023-10-05 10:48:37 -0500

    first commit

---
+++ src/main/java/org/ochafloquet/Main.java
@@ -0,0 +1,12 @@
+package org.ochafloquet;
+
+/**
+ * @project streamApi
+ * @autor Oscar Alfredo Chafloque Tampeck
+ * @date 05/10/2023
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
